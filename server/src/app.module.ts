398e2706968b739ecc79b07348b42dfffec1acb6
Ivan Chernov <chernoviv2013@gmail.com>
2021-02-28 17:19:02 +0200

    Added TypeORM and Postgres initial setup with .env variables

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// import { UsersModule } from './users/users.module';
+// import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+import { DbModule } from './db/db.module';
+// import { TesterModule } from './tester/tester.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+    DbModule,
+    // UsersModule,
+    // AuthModule,
+    // TesterModule,
+  ],
 })
 export class AppModule {}
