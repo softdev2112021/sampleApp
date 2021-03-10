ea10b9e69385c1bfad990872fbf7695d9d7c4915
Ivan Chernov <chernoviv2013@gmail.com>
2021-03-10 02:55:02 +0200

    Added fetch login and JWT saving to localstorage in login page

--- client/pages/auth/login.tsx
+++ client/pages/auth/login.tsx
@@ -1,9 +1,35 @@
-const Login = (props) => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // props.history.push("/dashboard/v3");
-  };
+import React, { useState, useEffect } from 'react';
+import { postData } from '../../lib/services/services';
+import Cache from '../../lib/cache/Cache';
 
+const cache = new Cache();
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value);
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value);
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const data = {
+      login: e.currentTarget.email.value,
+      password: e.currentTarget.password.value,
+    }
+
+    const res = await postData('http://localhost:4000/auth/login', JSON.stringify(data));
+
+    cache.write('token', await res.json());
+
+    if (res.ok) {
+      location.href = '/locations';
+    } else {
+      alert('Failed to login!');
+    }
+  }
+  
   return (
     <>
       <div className="login-cover">
@@ -17,7 +43,7 @@ const Login = (props) => {
       <div className="login login-v2">
         <div className="login-header">
           <div className="brand">
-            <span className="logo"></span> <b>Forecastic</b> App
+            <span className="logo"><i className="fas fa-cloud"></i></span> <b>Forecastic</b> App
             <small>Weather forecast by Ivan</small>
           </div>
           <div className="icon">
@@ -25,10 +51,13 @@ const Login = (props) => {
           </div>
         </div>
         <div className="login-content">
-          <form className="margin-bottom-0" onSubmit={handleSubmit}>
+          <form className="margin-bottom-0" onSubmit={onSubmit}>
             <div className="form-group m-b-20">
               <input
                 type="email"
+                name="email"
+                value={email}
+                onChange={onChangeEmail}
                 className="form-control form-control-lg"
                 placeholder="Email Address"
                 required
@@ -37,6 +66,9 @@ const Login = (props) => {
             <div className="form-group m-b-20">
               <input
                 type="password"
+                name="password"
+                value={password}
+                onChange={onChangePassword}
                 className="form-control form-control-lg"
                 placeholder="Password"
                 required
