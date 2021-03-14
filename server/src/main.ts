8ab515b39651ba8eef88a08bee7e0498e102d18a
Ivan Chernov <chernoviv2013@gmail.com>
2021-03-15 00:08:36 +0200

    Configured cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: `${process.env.CLIENT_HOST}:${process.env.CLIENT_PORT}`,
+    methods: 'GET,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(parseInt(process.env.PORT, 10) || 4000);
