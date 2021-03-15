577eb6e460ce864c133a051e6de5d7198549f543
Ivan Chernov <chernoviv2013@gmail.com>
2021-03-15 13:27:04 +0200

    Added cookie-parser

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+// import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  // app.use(csurf());
   app.enableCors({
     origin: `${process.env.CLIENT_HOST}:${process.env.CLIENT_PORT}`,
     methods: 'GET,POST,DELETE',
