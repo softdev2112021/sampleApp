47d7408f1ba88e8dfa8f411d11547f0c011de9aa
Ivan Chernov <chernoviv2013@gmail.com>
2021-03-15 13:25:17 +0200

    Added ClassSerializerInterceptor to exclude password form UserEntity

--- server/src/user/user.entity.ts
+++ server/src/user/user.entity.ts
@@ -1,14 +1,23 @@
+import { Exclude } from 'class-transformer';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { LocationEntity } from '../location/location.entity';
 
 @Entity()
 export class UserEntity {
+  constructor(partial: Partial<UserEntity>) {
+    Object.assign(this, partial);
+  }
+
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column({ nullable: false })
+  name: string;
+
+  @Column({ nullable: false })
   login: string;
 
+  @Exclude()
   @Column({ nullable: false })
   passwordHash: string;
 
