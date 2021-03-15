689e3c0c2da6cd1ada60946bac6b0562d0efd8a5
Ivan Chernov <chernoviv2013@gmail.com>
2021-03-15 21:47:30 +0200

    Improved convertTime function

--- client/lib/utils/convertTime/convertTime.ts
+++ client/lib/utils/convertTime/convertTime.ts
@@ -3,31 +3,32 @@ interface ConvertedTime {
   weekDay: string;
 }
 
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+const weekDays = [
+  "Sun",
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thu",
+  "Fri",
+  "Sat",
+];
+
 const convertTime = (timestamp: number): ConvertedTime => {
   const time = new Date(timestamp * 1000);
-  const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  const weekDays = [
-    "Sun",
-    "Mon",
-    "Tue",
-    "Wed",
-    "Thu",
-    "Fri",
-    "Sat",
-  ];
   const month = months[time.getMonth()];
   const weekDay = weekDays[time.getDay()];
   const date = `${month}/${time.getDate()}`;
