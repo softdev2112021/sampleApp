d1474bdcc91bc1d98268ac9a27b2340a935518ac
Ivan Chernov <chernoviv2013@gmail.com>
2021-03-16 22:22:22 +0200

    Added webpack props to nextjs config to avoid winston loading error

--- client/next.config.js
+++ client/next.config.js
@@ -12,4 +12,10 @@ module.exports = {
       },
     ]
   },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.node = {
+      fs: 'empty'
+    }
+    return config
+  },
 };
