48b65e5362e5de148482ffc25fb42a93b167774e
Ivan Chernov <chernoviv2013@gmail.com>
2021-03-17 18:20:02 +0200

    Added tests on server

--- server/src/location/location.controller.spec.ts
+++ server/src/location/location.controller.spec.ts
@@ -1,20 +1,153 @@
+import { Logger } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { ForecastController } from './location.controller';
-import { ForecastService } from './location.service';
+import { UserEntity } from '../user/user.entity';
+import { AddLocationDto } from './dto/addLocation.dto';
+import { DeleteLocationDto } from './dto/deleteLocation.dto';
+import { LocationController } from './location.controller';
+import { LocationEntity } from './location.entity';
+import { LocationService } from './location.service';
 
-describe('ForecastController', () => {
-  let controller: ForecastController;
+const location: LocationEntity = {
+  id: 1,
+  name: 'Dnipro',
+  coords: [48.450001, 34.98333],
+  forecast: {
+    current: {
+      dt: 1615980598,
+      temp: 7.89,
+      description: 'broken clouds',
+      icon: '04d',
+    },
+    daily: [
+      {
+        dt: 1616058000,
+        temp: {
+          min: 4.1,
+          max: 9.97,
+        },
+        pop: 0.6,
+        description: 'light rain',
+        icon: '10d',
+      },
+      {
+        dt: 1616144400,
+        temp: {
+          min: 1.01,
+          max: 4.49,
+        },
+        pop: 0.4,
+        description: 'light rain',
+        icon: '10d',
+      },
+      {
+        dt: 1616230800,
+        temp: {
+          min: 0.71,
+          max: 4.88,
+        },
+        pop: 0.84,
+        description: 'light snow',
+        icon: '13d',
+      },
+    ],
+  },
+};
+
+const user: UserEntity = {
+  id: 1,
+  name: 'name',
+  login: 'login',
+  passwordHash: 'hash',
+};
+
+const addLocationDto: AddLocationDto = {
+  name: 'name',
+  coords: [0, 0],
+};
+
+const deleteLocationDto: DeleteLocationDto = {
+  id: 1,
+};
+
+describe('LocationController', () => {
+  let controller: LocationController;
+  let service: LocationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [ForecastController],
-      providers: [ForecastService],
+      controllers: [LocationController],
+      providers: [
+        {
+          provide: LocationService,
+          useValue: {
+            getLocations: () => {},
+            addLocation: () => {},
+            deleteLocation: () => {},
+          },
+        },
+        {
+          provide: Logger,
+          useValue: { log: () => {} },
+        },
+      ],
     }).compile();
 
-    controller = module.get<ForecastController>(ForecastController);
+    controller = module.get<LocationController>(LocationController);
+    service = module.get<LocationService>(LocationService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('getLocations', () => {
+    it('should return locations array', async () => {
+      const getLocationsServiceResult = async (
+        user: UserEntity,
+      ): Promise<LocationEntity[]> => {
+        return [location];
+      };
+
+      const res = await getLocationsServiceResult(user);
+
+      jest
+        .spyOn(service, 'getLocations')
+        .mockImplementation(getLocationsServiceResult);
+
+      expect(await controller.getLocations(user)).toEqual(res);
+      expect(service.getLocations).toHaveBeenCalledWith(user);
+    });
+  });
+
+  describe('addLocation', () => {
+    it('should return a location', async () => {
+      const addLocationServiceResult = async (
+        user: UserEntity,
+        addLocationDto: AddLocationDto,
+      ): Promise<LocationEntity> => {
+        return location;
+      };
+
+      const res = await addLocationServiceResult(user, addLocationDto);
+
+      jest
+        .spyOn(service, 'addLocation')
+        .mockImplementation(addLocationServiceResult);
+
+      expect(await controller.addLocation(addLocationDto, user)).toEqual(res);
+    });
+  });
+
+  describe('deleteLocation', () => {
+    it('should return an empty promise', async () => {
+      const deleteLocationServiceResult = async (
+        user: UserEntity,
+        deleteLocationDto: DeleteLocationDto,
+      ): Promise<void> => {};
+
+      const res = await deleteLocationServiceResult(user, deleteLocationDto);
+
+      jest
+        .spyOn(service, 'deleteLocation')
+        .mockImplementation(deleteLocationServiceResult);
+
+      expect(await controller.deleteLocation(deleteLocationDto, user)).toEqual(res);
+    });
   });
 });
--- server/src/location/location.service.spec.ts
+++ server/src/location/location.service.spec.ts
@@ -1,18 +1,89 @@
+import { Logger } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { ForecastService } from './location.service';
+import { Coords } from '../weather/services/interfaces/coords.interface';
+import { Forecast } from '../weather/services/interfaces/forecast.interface';
+import { WeatherService } from '../weather/services/weather.service';
+import { LocationService } from './location.service';
 
-describe('ForecastService', () => {
-  let service: ForecastService;
+const forecast: Forecast = {
+  current: {
+    dt: 1615980598,
+    temp: 7.89,
+    description: 'broken clouds',
+    icon: '04d',
+  },
+  daily: [
+    {
+      dt: 1616058000,
+      temp: {
+        min: 4.1,
+        max: 9.97,
+      },
+      pop: 0.6,
+      description: 'light rain',
+      icon: '10d',
+    },
+    {
+      dt: 1616144400,
+      temp: {
+        min: 1.01,
+        max: 4.49,
+      },
+      pop: 0.4,
+      description: 'light rain',
+      icon: '10d',
+    },
+    {
+      dt: 1616230800,
+      temp: {
+        min: 0.71,
+        max: 4.88,
+      },
+      pop: 0.84,
+      description: 'light snow',
+      icon: '13d',
+    },
+  ],
+};
+
+const coords: Coords = [0, 0];
+
+describe('LocationService', () => {
+  let weatherService: WeatherService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ForecastService],
+      providers: [
+        {
+          provide: WeatherService,
+          useValue: { getWeatherForecast: () => {} },
+        },
+        {
+          provide: Logger,
+          useValue: { log: () => {} },
+        },
+      ],
     }).compile();
 
-    service = module.get<ForecastService>(ForecastService);
+    weatherService = module.get<WeatherService>(WeatherService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('getLocations', () => {
+    it('should return locations array', async () => {
+      const getWeatherServiceResult = async (
+        coords: Coords,
+      ): Promise<Forecast> => {
+        return forecast;
+      };
+
+      const res = await getWeatherServiceResult(coords);
+
+      jest
+        .spyOn(weatherService, 'getWeatherForecast')
+        .mockImplementation(getWeatherServiceResult);
+
+      expect(await weatherService.getWeatherForecast(coords)).toEqual(res);
+      expect(weatherService.getWeatherForecast).toHaveBeenCalledWith(coords);
+    });
   });
 });
